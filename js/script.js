06c0bed88a78b52d0d9485e9b51a5b4f7aca5d9e
Vlad1235 <fn1235@bk.ru>
2020-06-08 10:01:19 +0300

    slider logic

--- js/script.js
+++ js/script.js
@@ -317,4 +317,51 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 4000);
     }
 
+    // Slider logic
+
+    const slides = document.querySelectorAll(".offer__slide"),
+        previous = document.querySelector(".offer__slider-prev"),
+        next = document.querySelector(".offer__slider-next"),
+        total = document.querySelector("#total"),
+        current = document.querySelector("#current");
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    function addZero() {
+        if (slides.length < 10) {
+            total.textContent = `0${slides.length}`;
+        } else {
+            total.textContent = slides.length;
+        }
+    }
+    addZero();
+
+    function showSlides(n) {
+        if(n > slides.length) {
+            slideIndex = 1;
+        }
+        if(n < 1) {
+            slideIndex = slides.length;
+        }
+        slides.forEach(item => item.style.display= "none");
+        slides[slideIndex-1].style.display = "block";
+
+        if (slideIndex < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+    previous.addEventListener("click", () => {
+        plusSlides(-1);
+    });
+    next.addEventListener("click", ()=> {
+        plusSlides(1);
+    });
+
 });
