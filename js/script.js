356369de685acb8b4d82328c2a5c241c18ceb547
Vlad1235 <fn1235@bk.ru>
2020-06-08 15:18:04 +0300

    add rectangular dots at the bottom of slider window

--- js/script.js
+++ js/script.js
@@ -358,6 +358,9 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = "0.5");
+        dots[slideIndex-1].style.opacity = 1;
     });
 
     previous.addEventListener("click", ()=> {
@@ -379,6 +382,9 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             current.textContent = slideIndex;
         }
+
+        dots.forEach(dot => dot.style.opacity = "0.5");
+        dots[slideIndex-1].style.opacity = 1;
     });
 
     function addZero() {
@@ -392,5 +398,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     addZero();
 
+
+    // adding dots (rectangular format) at the bottom of window with sliders
+
+    const slider = document.querySelector(".offer__slider");
+    slider.style.position = "relative";
+
+    const indicators = document.createElement("ol"),
+        dots = [];
+
+    indicators.classList.add("carousel-indicators");
+    slider.append(indicators);
+
+    for(let i =0; i < slides.length; i++) {
+        const dot = document.createElement("li");
+        dot.setAttribute("data-slide-to", i+1);
+        dot.classList.add("dot");
+        if(i === 0) {
+            dot.style.opacity = 1;          
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    dots.forEach(dot => {
+        dot.addEventListener("click", (event) => {
+            const slideTo = event.target.getAttribute("data-slide-to");
+            
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length-2) * (slideTo - 1);
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            if(slideIndex < 10){
+                current.textContent = `0${slideIndex}`;
+            } else {
+                current.textContent = slideIndex;
+            }
+
+            dots.forEach(dot => dot.style.opacity = "0.5");
+            dots[slideIndex-1].style.opacity = 1;
+        });
+    });
 
 });
